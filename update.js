2928cf543e4d112ebfe9593b5081df7bc05436b5
Lucas Matías Ciruzzi <lucasmciruzzi@live.com>
2016-02-03 18:59:08 -0300

    Updated to Font Awesome 4.5.0 and update.js to ES2015

--- update.js
+++ update.js
@@ -1,13 +1,34 @@
 /* Updates fa-icons.html with latest Font Awesome icons */
 
-var // Node Modules
-	extend = require("extend"),
-	fs = require("fs"),
-	Handlebars = require("handlebars"),
-	request = require("request"),
-	yaml = require("js-yaml");
+// Node Modules
+const request = require("request");
+const { safeLoad } = require("js-yaml");
+const { writeFileSync } = require("fs");
 
-var // Request options (proxy?)
+var // Dedent for Template Strings: https://gist.github.com/zenparsing/5dffde82d9acef19e43c
+	dedent = (callSite, ...args) => {
+		const format = str => {
+			let size = -1;
+			return str.replace(/\n(\s+)/g, (m, m1) => {
+				if (size < 0) {
+					size = m1.replace(/\t/g, "	").length;
+				}
+				return "\n" + m1.slice(Math.min(m1.length, size));
+			});
+		};
+		if (typeof callSite === "string") {
+			return format(callSite);
+		}
+		if (typeof callSite === "function") {
+			return (...args) => format(callSite(...args));
+		}
+		let output = callSite
+			.slice(0, args.length + 1)
+			.map((text, i) => (i === 0 ? "" : args[i - 1]) + text)
+			.join("");
+		return format(output);
+	},
+	// Request options (proxy?)
 	requestOptions = {
 		proxy: ""
 	},
@@ -15,40 +36,48 @@ var // Request options (proxy?)
 	rawRepo = "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/",
 	// Request urls
 	urls = {
-		yml: rawRepo + "src/icons.yml",
-		svg: rawRepo + "src/assets/font-awesome/fonts/fontawesome-webfont.svg"
+		yml: `${rawRepo}src/icons.yml`,
+		svg: `${rawRepo}src/assets/font-awesome/fonts/fontawesome-webfont.svg`
 	},
 	// Base icon width
 	iconWidth = 1792,
 	// Output SVG object
 	svg = {
-		begin: Handlebars.compile("<!--\n" +
-			"Polymer icon set generated from Font Awesome SVG Font\n" +
-			"https://github.com/vangware/fontawesome-iconset\n\n" +
-			"@element iron-iconset-svg\n" +
-			"@demo demo.html\n" +
-			"-->\n" +
-			"<link rel=\"import\" href=\"../iron-icon/iron-icon.html\">\n" +
-			"<link rel=\"import\" href=\"../iron-iconset-svg/iron-iconset-svg.html\">\n" +
-			"<iron-iconset-svg name=\"{{name}}\" size=\"" + iconWidth + "\">\n" +
-			"<svg><defs>\n"),
-		defs: {},
-		end: "</defs></svg>\n</iron-iconset-svg>"
+		begin: ({ name }) => dedent`<!--
+			Polymer icon set generated from Font Awesome SVG Font
+			https://github.com/vangware/fontawesome-iconset
+			@element iron-iconset-svg
+			@demo demo.html
+			-->
+			<link rel="import" href="../iron-icon/iron-icon.html">
+			<link rel="import" href="../iron-iconset-svg/iron-iconset-svg.html">
+			<iron-iconset-svg name="${name}" size="${iconWidth}">
+			<svg><defs>
+		`,
+		defs: Object.create(null),
+		end: "</defs></svg></iron-iconset-svg>"
 	},
 	// Icon template
-	template = Handlebars.compile("<g id=\"{{name}}\" transform=\"scale({{scaleX}} {{scaleY}}) translate({{shiftX}} {{shiftY}})\"><path d=\"{{path}}\"/></g>\n"),
+	template = ({
+		name,
+		path,
+		scaleX,
+		scaleY,
+		shiftX,
+		shiftY
+	}) => `<g id="${name}" transform="scale(${scaleX} ${scaleY}) translate(${shiftX} ${shiftY})"><path d="${path}"/></g>`,
 	// Icons object
-	icons = {},
+	icons = Object.create(null),
 	// Pixel base size
 	pixelBase = 128,
 	// Generate icon and addit to output svg
-	generateIcon = function (iconData, svgPath, params) {
+	generateIcon = (iconData, svgPath, params) => {
 		"use strict";
 		var size = iconWidth / params.horizAdvX,
 			shiftX = -(-(iconWidth - params.horizAdvX) / 2),
 			def;
 		size = size > 1 ? 1 : size;
-		def = template(extend({
+		def = template(Object.assign(Object.create(null), {
 			name: iconData.id,
 			path: svgPath
 		}, params, {
@@ -57,7 +86,7 @@ var // Request options (proxy?)
 			shiftX: shiftX < 0 ? 0 : shiftX,
 			shiftY: -(1280 + 2 * pixelBase)
 		}));
-		iconData.categories.forEach(function (category) {
+		iconData.categories.forEach(category => {
 			if (svg.defs[category]) {
 				svg.defs[category].push(def);
 			} else {
@@ -67,29 +96,29 @@ var // Request options (proxy?)
 	};
 
 console.log("Request YML ...");
-request(extend(true, requestOptions, {
+request(Object.assign(Object.create(null), requestOptions, {
 	url: urls.yml
-}), function (ymlError, ymlResponse, iconsYaml) {
+}), (ymlError, ymlResponse, iconsYaml) => {
 	"use strict";
 	console.log("Request SVG ...");
-	request(extend(true, requestOptions, {
+	request(Object.assign(Object.create(null), requestOptions, {
 		url: urls.svg
-	}), function (svgError, svgResponse, fontData) {
-		var yamlIcons = yaml.safeLoad(iconsYaml).icons,
+	}), (svgError, svgResponse, fontData) => {
+		var yamlIcons = safeLoad(iconsYaml).icons,
 			lines = fontData.toString("utf8").split("\n");
-		yamlIcons.forEach(function (icon) {
+		yamlIcons.forEach(icon => {
 			var categories = icon.categories;
 			icons[icon.unicode] = {
 				id: icon.id,
 				categories: ["all"]
 			};
-			categories.forEach(function(category) {
+			categories.forEach(category => {
 				var normalizedCategory = category.toLowerCase().replace(/ /g, "-").replace("-icons", "");
 				icons[icon.unicode].categories.push(normalizedCategory);
 			});
 		});
 		console.log("Parsing icons ...");
-		lines.forEach(function (line) {
+		lines.forEach(line => {
 			var match = line.match(/^<glyph unicode="&#x([^"]+);"\s*(?:horiz-adv-x="(\d+)")?\s*d="([^"]+)"/),
 				unicode, svgPath, horizAdvX;
 			if (match) {
@@ -104,8 +133,10 @@ request(extend(true, requestOptions, {
 			}
 		});
 		console.log("Writing icons to files ...");
-		for (var def in svg.defs) {
-			fs.writeFileSync("fa-" + def + ".html", svg.begin({ name: (def === "all") ? "fa" : ("fa-" + def) }) + svg.defs[def].join("") + svg.end);
-		}
+		Object.keys(svg.defs).forEach(def => {
+			writeFileSync(`fa-${def}.html`, svg.begin({
+				name: (def === "all") ? "fa" : ("fa-" + def)
+			}) + svg.defs[def].join("") + svg.end);
+		})
 	});
 });
